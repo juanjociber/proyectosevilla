539d277634b8e10e8cebfb37f60ddeb5708b144b
juanjociber <i202015638@cibertec.edu.pe>
2021-03-03 19:21:22 -0500

    proyecto arte

---
+++ js/evento.js
@@ -0,0 +1,21 @@
+/**EVENTO ELEMENTO CARD */
+var card = document.getElementsByClassName('card');
+for(var i = 0 ; i < card.length ; i++){
+    card[i].onmouseover=function(){
+        $(this).children('div').children('p').css('color','var(--blanco)');
+    }
+    card[i].onmouseout=function(){
+        $(this).children('div').children('p').css('color','var(--amarillo)');
+        
+    }
+}
+/**EVENTO REDES */
+var redes = document.getElementsByClassName('social');
+for(var i = 0 ; i < redes.length ; i++ ){
+    redes[i].onmouseover=function(){
+        $(this).css('transform','scale(1.15)');
+    }
+    redes[i].onmouseout=function(){
+        $(this).css('transform','scale(1)');
+    }
+}
